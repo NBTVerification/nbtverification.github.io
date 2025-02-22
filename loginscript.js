2ddef7aa917f7b507e07ed119090424f45650c10
NothingButTyler <tylerbloxofficialyt@gmail.com>
2025-02-21 20:25:40 -0700

    Create loginscript.js

---
+++ loginscript.js
@@ -0,0 +1,17 @@
+// script.js
+document.getElementById('loginForm').addEventListener('submit', function(e) { e.preventDefault(); 
+                                                                             var username = document.getElementById('username').value;
+                                                                             var password = document.getElementById('password').value;
+                                                                             var errorMsg = document.getElementById('error-msg'); 
+                                                                             if (username === '' || password === '') { errorMsg.textContent = 'Username or password is incorrect, please try again.';
+                                                                                                                      errorMsg.classList.remove('hidden');
+                                                                                                                     } 
+                                                                             else {
+                                                                               // Simulate a call to Google reCAPTCHA v3 
+                                                                               alert('Please complete Google reCAPTCHA v3');
+                                                                               // Optionally you can reset form after alert
+                                                                               // 
+                                                                               e.target.reset();
+                                                                               // 
+                                                                               errorMsg.classList.add('hidden');
+                                                                             } });
